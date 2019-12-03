a22ecd09ea0c1455587e365a3fc4d7828270842e
Sara <hey@iamsaravieira.com>
2019-12-03 12:51:05 +0100

    fix language

--- src/index.js
+++ src/index.js
@@ -1,46 +1,23 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import React from "react";
 import Checkout from "./Checkout";
 import ThankYou from "./ThankYou";
 import Styles from "./utils/styles";
 import { Router } from "@reach/router";
 import Header from "./components/header";
 import "./i18n";
-import { Bars } from "svg-loaders-react";
 
-const Main = () => {
-  const { i18n } = useTranslation();
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    fetch(
-      "https://api.ipgeolocation.io/ipgeo?apiKey=891717abedb44a3aaab0fd1f8bda0fad"
-    )
-      .then(rsp => rsp.json())
-      .then(response => {
-        i18n.changeLanguage(response.languages);
-        setLoading(false);
-      });
-  }, [i18n]);
-
-  return loading ? (
-    <div className="loading">
-      <Styles />
-      <Bars fill="#FA8D0F" />
-    </div>
-  ) : (
-    <>
-      <Styles />
-      <Header />
-      <Router>
-        <App path="/" />
-        <Checkout path="/checkout" />
-        <ThankYou path="/thank-you" />
-      </Router>
-    </>
-  );
-};
+const Main = () => (
+  <>
+    <Styles />
+    <Header />
+    <Router>
+      <App path="/" />
+      <Checkout path="/checkout" />
+      <ThankYou path="/thank-you" />
+    </Router>
+  </>
+);
 
 ReactDOM.render(<Main />, document.getElementById("root"));
