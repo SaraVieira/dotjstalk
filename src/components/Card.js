b505aa25e8cd23da95e88f6491bae31364618c68
Sara <hey@iamsaravieira.com>
2019-12-05 10:29:15 +0100

    fix layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,48 @@ import React from "react";
 import amexIcon from "../images/amex.svg";
 import discoverIcon from "../images/discover.svg";
 import visaIcon from "../images/visa.svg";
-import mastercardIcon from "../images/mastercard.svg";
+import masterCardIcon from "../images/mastercard.svg";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  width: auto;
+  height: auto;
+  padding: 0;
+  margin: 0;
+  border: none;
+
+  :hover {
+    background: transparent;
+  }
+`;
 
 export default ({ type, onClick }) => {
   switch (type) {
     case "American Express":
-      return <img onClick={onClick} src={amexIcon} alt="American Express" />;
+      return (
+        <Button onClick={onClick}>
+          <img src={amexIcon} alt="American Express" />
+        </Button>
+      );
     case "Mastercard":
-      return <img onClick={onClick} src={mastercardIcon} alt="Mastercard" />;
+      return (
+        <Button onClick={onClick}>
+          <img src={masterCardIcon} alt="Mastercard" />
+        </Button>
+      );
     case "Visa":
-      return <img onClick={onClick} src={visaIcon} alt="Visa" />;
+      return (
+        <Button onClick={onClick}>
+          <img src={visaIcon} alt="Visa" />
+        </Button>
+      );
     case "Discover":
-      return <img onClick={onClick} src={discoverIcon} alt="Discover" />;
+      return (
+        <Button onClick={onClick}>
+          <img src={discoverIcon} alt="Discover" />
+        </Button>
+      );
     default:
       return null;
   }
